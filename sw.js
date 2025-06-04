2ac1557539975dce6d9ba9e3eb1a98f27bc526b7
DH.L <meozigoon@gmail.com>
2025-06-05 08:26:51 +0900

    v17.4
    manifest.json: 수정된 start_url 경로를 '/index.html'로 변경
    sw.js: 서비스 워커 설치 및 가져오기 이벤트 핸들러 추가

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-self.addEventListener("fetch", e => {});
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('v1').then(cache =>
+      cache.addAll([
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/image/hssh_Logo.png',
+        '/image/hssh_Logo_192.png',
+        '/image/hssh_Logo_512.png',
+      ])
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
